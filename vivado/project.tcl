c432519348011f22e77de6f274571fd07ea1ec88
Michael Mattioli <mmattioli@users.noreply.github.com>
2017-02-28 22:00:57 -0500

    Build Vivado project in vivado directory
    The Tcl script used to build the Vivado project was previously building
    it in a subdirectory named "project" so the path to the Vivado project
    folder would be vivado/project/. The Tcl script now builds it directly
    in vivado/ .

--- vivado/project.tcl
+++ vivado/project.tcl
@@ -93,7 +93,7 @@ if { $::argc > 0 } {
 set orig_proj_dir "[file normalize "$origin_dir/"]"
 
 # Create project
-create_project project ./project -part xc7z020clg484-1
+create_project project . -part xc7z020clg484-1
 
 # Set the directory path for the new project
 set proj_dir [get_property directory [current_project]]
